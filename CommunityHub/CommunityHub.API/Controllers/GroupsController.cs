eefcde4a7c4c5d06cebffac8521f9267f9886b6d
Vikvillka <bechkovskayavic@gmail.com>
2025-09-16 14:40:54 +0300

    fixed status codes. Simplified controller for the new version

--- CommunityHub/CommunityHub.API/Controllers/GroupsController.cs
+++ CommunityHub/CommunityHub.API/Controllers/GroupsController.cs
@@ -2,56 +2,56 @@ using CommunityHub.API.Initialization;
 using CommunityHub.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CommunityHub.API.Controllers
+namespace CommunityHub.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GroupsController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class GroupsController : ControllerBase
+    [HttpGet("getAll")]
+    [ProducesResponseType(typeof(List<Group>), StatusCodes.Status200OK)]
+    public IActionResult GetAll()
     {
-        [HttpGet("getAll")]
-        [ProducesResponseType(typeof(List<Group>), StatusCodes.Status200OK)]
-        public IActionResult GetAll()
-        {
-            return Ok(DataStorage.GetAllGroups());
-        }
+        return Ok(DataStorage.GetAllGroups());
+    }
 
-        [HttpGet("get/{id}")]
-        [ProducesResponseType(typeof(Group), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult GetById(Guid id)
-        {
-            var group = DataStorage.GetGroupById(id);
-            if (group == null) return NotFound();
-            return Ok(group);
-        }
+    [HttpGet("get/{id}")]
+    [ProducesResponseType(typeof(Group), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult GetById(Guid id)
+    {
+        var group = DataStorage.GetGroupById(id);
+        if (group == null) return NotFound();
+        return Ok(group);
+    }
 
-        [HttpPost("create")]
-        [ProducesResponseType(typeof(Group), StatusCodes.Status201Created)]
-        public IActionResult Create([FromBody] Group group)
-        {
-            DataStorage.AddGroup(group);
-            return CreatedAtAction(nameof(GetById), new { id = group.Id }, group);
-        }
+    [HttpPost("create")]
+    [ProducesResponseType(typeof(Group), StatusCodes.Status200OK)]
+    public IActionResult Create([FromBody] Group group)
+    {
+        DataStorage.AddGroup(group);
+        return Ok(group);
+    }
 
-        [HttpPut("update")]
-        [ProducesResponseType(typeof(Group), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult Update([FromBody] Group updateGroup)
-        {
-            var success = DataStorage.UpdateGroup(updateGroup);
-            if (!success) return NotFound();
+    [HttpPut("update")]
+    [ProducesResponseType(typeof(Group), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult Update([FromBody] Group updateGroup)
+    {
+        var success = DataStorage.UpdateGroup(updateGroup);
+        if (!success) return BadRequest();
 
-            return Ok(updateGroup);
-        }
+        return Ok();
+    }
 
-        [HttpDelete("delete/{id}")]
-        [ProducesResponseType(typeof(Group), StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult DeleteById(Guid id) 
-        {
-            var success = DataStorage.DeleteGroup(id);
-            if (!success) return NotFound();
-            return NoContent();
-        }
+    [HttpDelete("delete/{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult DeleteById(Guid id)
+    {
+        var success = DataStorage.DeleteGroup(id);
+        if (!success) return BadRequest();
+        return NoContent();
     }
 }
+
