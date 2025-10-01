e2c6fe011d978f724eed8cb483bc577752d4b4ba
Vikvillka <bechkovskayavic@gmail.com>
2025-10-01 16:02:49 +0300

    Transferred interfaces to the Interfaces layer with current namespaces

---
+++ CommunityHub/CommunityHub.Interfaces/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using CommunityHub.Domain.Entities;
+
+namespace CommunityHub.Interfaces.Interfaces.Repositories;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<List<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(Guid id);
+    Task<T> CreateAsync(T entity);
+    Task<bool> UpdateAsync(T entity);
+    Task<bool> DeleteAsync(Guid id);
+    IQueryable<T> AsQueryable();
+}
---
+++ CommunityHub/CommunityHub.Interfaces/Interfaces/Repositories/ITagRepository.cs
@@ -0,0 +1,8 @@
+﻿using CommunityHub.Domain.Entities;
+
+namespace CommunityHub.Interfaces.Interfaces.Repositories;
+
+public interface ITagRepository : IRepository<EventTag>
+{
+    Task<List<EventTag>> GetByIdsAsync(List<Guid> tagIds);
+}
