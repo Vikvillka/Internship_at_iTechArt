431556e904f89e438fec7c4123797c66cdb6ced8
Vikvillka <bechkovskayavic@gmail.com>
2025-10-08 15:47:03 +0300

    Added UserRepository with Interface

---
+++ CommunityHub/CommunityHub.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using CommunityHub.Application.Interfaces.Repositories;
+using CommunityHub.Domain.Entities;
+using CommunityHub.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommunityHub.Infrastructure.Repositories;
+
+public class UserRepository : EfRepository<User>, IUserRepository
+{
+    public UserRepository(CommunityHubDbContext context) : base(context)
+    {
+    }
+
+    public async Task<User?> GetByUsernameAsync(string username)
+    {
+        return await _dbSet.FirstOrDefaultAsync(u => u.Username == username);
+    }
+}
