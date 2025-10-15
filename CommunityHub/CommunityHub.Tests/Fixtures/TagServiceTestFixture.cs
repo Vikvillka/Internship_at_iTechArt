fa6afd3aa41b47137d6a911747133fad43298078
Vikvillka <bechkovskayavic@gmail.com>
2025-10-15 19:28:43 +0300

    Added tests for TagService methods

---
+++ CommunityHub/CommunityHub.Tests/Fixtures/TagServiceTestFixture.cs
@@ -0,0 +1,26 @@
+﻿using CommunityHub.Application.Interfaces.Repositories;
+using CommunityHub.Application.Services;
+using CommunityHub.Domain.Entities;
+using Moq;
+
+namespace CommunityHub.Tests.Fixtures
+{
+    public class TagServiceTestFixture
+    {
+        public TagService Service { get; }
+        public Mock<ITagRepository> MockRepo { get; }
+        public List<EventTag> Tags { get; }
+
+        public TagServiceTestFixture()
+        {
+            MockRepo = new Mock<ITagRepository>();
+            Service = new TagService(MockRepo.Object);
+
+            Tags =
+            [
+                new() { Id = Guid.NewGuid(), Name = "TagA" },
+                new() { Id = Guid.NewGuid(), Name = "TagB" }
+            ];
+        }
+    }
+}
