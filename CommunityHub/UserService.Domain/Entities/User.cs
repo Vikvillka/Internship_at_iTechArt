5d874656f6b090590366d4e550807848c09a5548
Vikvillka <bechkovskayavic@gmail.com>
2025-11-09 21:59:10 +0300

    Separation on UserService, added domain layer

---
+++ CommunityHub/UserService.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using UserService.Domain.Enums;
+
+namespace UserService.Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public Gender Gender { get; set; }
+    public string City { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+}
