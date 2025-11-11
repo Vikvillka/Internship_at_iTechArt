ebc4f2eabfac8512b72e1242271fda83f2ccd652
Vikvillka <bechkovskayavic@gmail.com>
2025-11-11 15:56:20 +0300

    Added repositories to UserService

---
+++ CommunityHub/UserService.Application/Intarfaces/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using UserService.Domain.Entities;
+
+namespace UserService.Application.Intarfaces.Repositories;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<List<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(Guid id);
+    Task<T> CreateAsync(T entity);
+    Task<bool> UpdateAsync(T entity);
+    Task<bool> DeleteAsync(Guid id);
+    IQueryable<T> AsQueryable();
+}
