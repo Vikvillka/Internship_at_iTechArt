ec4b6fc6877a943a4e561768ca6cda72734f360b
Vikvillka <bechkovskayavic@gmail.com>
2025-11-12 18:23:02 +0300

    Added DTOs to CommunitySub and EventPart, added mappings

---
+++ CommunityHub/UserService.API/Extensions/Mappings/Profiles/EventParticipationProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using UserService.Contracts.DTOs.ParticipationDTOs;
+using UserService.Domain.Entities;
+
+namespace UserService.API.Extensions.Mappings.Profiles;
+
+public class EventParticipationProfile : Profile
+{
+    public EventParticipationProfile()
+    {
+        CreateMap<CreateEventParticipationRequest, EventParticipation>();
+    }
+}
